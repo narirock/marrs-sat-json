f3be28e0b05dd6ee0bf9119dd424fbdcf9bf98e5
narirock <nari9rock@hotmail.com>
2020-11-25 18:15:41 -0300

    filtros

--- index.js
+++ index.js
@@ -12,73 +12,99 @@ const path = require('path')
 const directory = process.env.DIRECTORY//informar a pasta onde estão os arquivos
 const directoryPath = path.join(directory);
 const xml2js = require('xml2js');
-const json = [];
 
-watch(directory, { recursive: true }, function(evt, name) {
+
+watch(directory, { recursive: true }, function (evt, name) {
+    //console.log(evt);
+    //return null;
     console.log('%s changed.', name);
-    
-    const xml = fs.readFileSync(name, 'utf8');
-    xml2js.parseString(xml, (err, result) => {
-        if(err) {
-            throw err;
-        }
-        //console.log(result.CFe)
-        const json = {
-            "file": name,
-            "cNF": result.CFe.infCFe[0].ide[0].cNF[0],
-            "CNPJ": result.CFe.infCFe[0].ide[0].CNPJ[0],
-            "nCFe": result.CFe.infCFe[0].ide[0].nCFe[0],
-            "dEmi": result.CFe.infCFe[0].ide[0].dEmi[0],
-            "total": result.CFe.infCFe[0].total[0].vCFe[0]
-        };
+    if (name.includes('.xml') && evt != "remove") {
+        const xml = fs.readFileSync(name, 'utf8');
+        xml2js.parseString(xml, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            //console.log(result.CFe)
+            const json = {
+                "file": name,
+                "cNF": result.CFe.infCFe[0].ide[0].cNF[0],
+                "CNPJ": result.CFe.infCFe[0].ide[0].CNPJ[0],
+                "nCFe": result.CFe.infCFe[0].ide[0].nCFe[0],
+                "dEmi": result.CFe.infCFe[0].ide[0].dEmi[0],
+                "total": result.CFe.infCFe[0].total[0].vCFe[0]
+            };
 
-        console.log(json); 
+            console.log(json);
 
-        var headersOpt = {  
-            "content-type": "application/json",
-        };
-        request(
+            var headersOpt = {
+                "content-type": "application/json",
+            };
+            request(
                 {
-                method:'post',
-                url:process.env.MARRS_API + '/sat', 
-                form: json, 
-                headers: headersOpt,
-                json: true,
-            }, function (error, response, body) {  
-                //Print the Response
-                console.log(body);  
-        }); 
-
-
-              
-    });
+                    method: 'post',
+                    url: process.env.MARRS_API + '/sat',
+                    form: json,
+                    headers: headersOpt,
+                    json: true,
+                }, function (error, response, body) {
+                    //Print the Response
+                    console.log(body);
+                });
+        });
+    } else {
+        console.log('no xml');
+    }
 });
 
-
-
-
-
 app.get('/', (req, res) => {
-    fs.readdir(directoryPath, function (err, files) {
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        } 
-        files.forEach(function (file) {
-            const xml = fs.readFileSync(directory + '/' + file, 'utf8');
-            xml2js.parseString(xml, (err, result) => {
-                if(err) {
-                    throw err;
-                }
-                //console.log(result.CFe.infCFe[0].ide[0].dEmi)
-                json.push({'file':file, 'cNF':result.CFe.infCFe[0].ide[0].cNF[0],'nCFe':result.CFe.infCFe[0].ide[0].nCFe[0], 'dEmi':result.CFe.infCFe[0].ide[0].dEmi[0], 'total':result.CFe.infCFe[0].total[0].vCFe[0]});
-                
+    const json = [];
+    if (req.query.date) {
+        let date = req.query.date.split("-").join("/");
+
+        fs.readdir(directoryPath + "/" + date, function (err, files) {
+            if (err) {
+                return console.log('Unable to scan directory: ' + err);
+            } 
+            files.forEach(function (file) {
+                const xml = fs.readFileSync(directory + '/' + file, 'utf8');
+                xml2js.parseString(xml, (err, result) => {
+                    if(err) {
+                        throw err;
+                    }
+                    if (file.includes('.xml')) {
+                        var sat = {
+                            'file': file,
+                            'cNF': result.CFe.infCFe[0].ide[0].cNF[0],
+                            "CNPJ": result.CFe.infCFe[0].ide[0].CNPJ[0],
+                            'nCFe': result.CFe.infCFe[0].ide[0].nCFe[0],
+                            'dEmi': result.CFe.infCFe[0].ide[0].dEmi[0],
+                            'total': result.CFe.infCFe[0].total[0].vCFe[0]
+                        };
+
+                        if (req.query.number) {
+                            //console.log('validar numero ' + req.query.number);
+                            //console.log(result.CFe.infCFe[0].ide[0].nCFe[0]);
+                            if (req.query.number === result.CFe.infCFe[0].ide[0].nCFe[0]) {
+                                //console.log('achou');
+                                json.push(sat);
+                            }
+                        } else {
+                            json.push(sat);
+                        }
+                    }
+                    
+                });
             });
+            res.send(JSON.stringify(json));
         });
+
+        //console.log(date);
+
+    } else {
         res.send(JSON.stringify(json));
-    });
+    }
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-    
+    console.log(`Server rodando em http://localhost:${port}`)
 })
