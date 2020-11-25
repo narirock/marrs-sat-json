023d2286b579cac0266b5fe8c5f6de0ff9319008
narirock <nari9rock@hotmail.com>
2020-11-24 21:06:28 -0300

    criando sinc de arquivos

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
-const express = require('express')
+
 require("dotenv").config();
+const express = require('express')
 const app = express()
-const port = 3001
+var request = require('request');
+const port = 3003
 
 /* libs para leitura de diretorios e arquivos */
-
+var watch = require('node-watch');
 const fs = require('fs')
 const path = require('path')
 const directory = process.env.DIRECTORY//informar a pasta onde estão os arquivos
 const directoryPath = path.join(directory);
 const xml2js = require('xml2js');
 const json = [];
+
+watch(directory, { recursive: true }, function(evt, name) {
+    console.log('%s changed.', name);
+    
+    const xml = fs.readFileSync(name, 'utf8');
+    xml2js.parseString(xml, (err, result) => {
+        if(err) {
+            throw err;
+        }
+        //console.log(result.CFe)
+        const json = {
+            "file": name,
+            "cNF": result.CFe.infCFe[0].ide[0].cNF[0],
+            "CNPJ": result.CFe.infCFe[0].ide[0].CNPJ[0],
+            "nCFe": result.CFe.infCFe[0].ide[0].nCFe[0],
+            "dEmi": result.CFe.infCFe[0].ide[0].dEmi[0],
+            "total": result.CFe.infCFe[0].total[0].vCFe[0]
+        };
+
+        console.log(json); 
+
+        var headersOpt = {  
+            "content-type": "application/json",
+        };
+        request(
+                {
+                method:'post',
+                url:process.env.MARRS_API + '/sat', 
+                form: json, 
+                headers: headersOpt,
+                json: true,
+            }, function (error, response, body) {  
+                //Print the Response
+                console.log(body);  
+        }); 
+
+
+              
+    });
+});
+
+
+
+
 
 app.get('/', (req, res) => {
     fs.readdir(directoryPath, function (err, files) {
